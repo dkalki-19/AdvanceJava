cb13231ad98d2a45200133293a5f46a889711147
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-24 14:22:31 +0530

    Updating usecase one

--- src/JavaRegex/UserRegistration.java
+++ src/JavaRegex/UserRegistration.java
@@ -1,5 +1,9 @@
 package JavaRegex;
 
 public class UserRegistration {
-
+	// UC1: First Name
+    public boolean validateFirstName(String firstName) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        return firstName.matches(regex);
+    }
 }
